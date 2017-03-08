ffbd0a80b250d9d6efb693e958364b99d6034d38
Małgosia Krzemińska <malgosia.krzeminska@yahoo.pl>
2017-03-08 11:12:08 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var a = 5
+var h = 4
+var triangleArea = a*h/2
+var answer = prompt("Pole trójkąta o podstawie a: 5 i wysokości h: 4 wynosi: 10")
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
